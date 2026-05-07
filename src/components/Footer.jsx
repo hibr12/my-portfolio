b205f8376bce2ae8756ec76582bdb18534e4f4a0
hibr12 <hb.yw12@gmail.com>
2026-05-07 11:16:06 +0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,18 @@ function Footer() {
       <div>
         <strong>hibru yitayew</strong>
         <p>Computer Science Student</p>
+        <p>at bahir dar university</p>
       </div>
       <div className="footer__links">
         <a href="https://github.com/hibr12" target="_blank" rel="noreferrer">
           GitHub
-        </a>
+        </a><br />
         <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noreferrer">
           LinkedIn
         </a>
-        <div className="phone">
-          <a href="tel:+251926673294">+251 926 673 294</a>
-        </div>
+        <br />
+          <a href="tel:+251926673294">phone: +251 926 673 294</a>
+        
       </div>
       <p>Copyright © 2026 hibru yitayew. All rights reserved.</p>
     </footer>
